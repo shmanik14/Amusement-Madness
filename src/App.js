1564d7945c105ac974dddd1549a28078a98abdbf
sahadat <shmanik14@yahoo.com>
2021-03-10 23:03:01 +0600

    add all component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import NoMatch from './Components/NoMatch/NoMatch';
+import Home from './Components/Home/Home';
+import LeagueDetails from './Components/LeagueDetails/LeagueDetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route to path="/league/:leagueId">
+              <LeagueDetails></LeagueDetails>
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+      </Router>     
     </div>
   );
 }
